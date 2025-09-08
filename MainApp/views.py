aa3c8216931203d413f7d327f5b3897cd6af1b2a
Vladimir Vasin <ossminoga88@gmail.com>
2025-09-08 20:49:19 +0300

    Done. M1:task2-4

--- MainApp/views.py
+++ MainApp/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+author = {
+    "Имя": "Владимир",
+    "Отчество": "Сергеевич",
+    "Фамилия": "Васин",
+    "телефон": "8-916-678-85-17",
+    "email": "ossminoga88@gmail.com",
+}
+
 
 def home(request):
     text = """
@@ -9,3 +17,17 @@ def home(request):
     <strong>Автор</strong>: <i>Васин В.С.</i>
     """
     return HttpResponse(text)
+
+
+def about(request):
+    text = f"""
+        Имя: {author['Имя']}<br>
+        Отчество: {author['Отчество']}<br>
+        Фамилия: {author['Фамилия']}<br>
+        телефон: {author['телефон']}<br>
+        email: {author['email']}<br>
+        """
+    return HttpResponse(text)
+
+def get_item(request, item_id: str):
+    return HttpResponse(item_id)
