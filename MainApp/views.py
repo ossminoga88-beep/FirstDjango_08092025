c0ab0ae6d2ac26e1871d24dfbadf7cfb3cefb589
Vladimir Vasin <ossminoga88@gmail.com>
2025-09-08 20:04:39 +0300

    Done. M1:task1

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
+
+def home(request):
+    text = """
+    <h1>"Изучаем django"</h1>
+    <strong>Автор</strong>: <i>Васин В.С.</i>
+    """
+    return HttpResponse(text)
