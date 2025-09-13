c263eb490615e77b139d28568e135ce94568c004
Vladimir Vasin <ossminoga88@gmail.com>
2025-09-13 16:38:52 +0300

    Add fixtures

--- MainApp/views.py
+++ MainApp/views.py
@@ -45,7 +45,7 @@ def about(request):
 
 
 def get_item(request, item_id: int):
-    """По указанному id возвращает элемент из списка"""
+    """TODO: get item by id from db."""
     for item in items:
         if item['id'] == item_id:
             context = {
@@ -56,6 +56,7 @@ def get_item(request, item_id: int):
         
 
 def get_items(request):
+    """TODO: get all items from db."""
     context = {"items": items }
     return render(request, "items_list.html", context)
 
