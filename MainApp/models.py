4ec2019c28e04913681fb78045db929c0e2ddd3d
Vladimir Vasin <ossminoga88@gmail.com>
2025-09-15 15:28:29 +0300

    Done.M3:part2, task 1-3

--- MainApp/models.py
+++ MainApp/models.py
@@ -6,5 +6,7 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
     count = models.PositiveIntegerField()
+    description = models.TextField(max_length=300, default="Описание элемента")
+
 
 
