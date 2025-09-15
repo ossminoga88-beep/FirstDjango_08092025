0f8bc48bfb2eec83a9de1717d13e08011af52a8d
Vladimir Vasin <ossminoga88@gmail.com>
2025-09-15 13:58:52 +0300

    Done.M3:task1-4

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
+from MainApp.models import Item
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 
 
@@ -12,13 +14,13 @@ author = {
 }
 
 
-items = [
-    {"id": 1, "name": "Кроссовки adidas", "quantity":5},
-    {"id": 2, "name": "Куртка кожаная", "quantity":2},
-    {"id": 5, "name": "Coca-cola 1 литр", "quantity":12},
-    {"id": 7, "name": "Картофель фри", "quantity":0},
-    {"id": 8, "name": "Кепка", "quantity":124},
-]
+# items = [
+#     {"id": 1, "name": "Кроссовки adidas", "quantity":5},
+#     {"id": 2, "name": "Куртка кожаная", "quantity":2},
+#     {"id": 5, "name": "Coca-cola 1 литр", "quantity":12},
+#     {"id": 7, "name": "Картофель фри", "quantity":0},
+#     {"id": 8, "name": "Кепка", "quantity":124},
+# ]
 
 
 def home(request) -> HttpResponse:
@@ -38,26 +40,24 @@ def about(request):
         "phone": "8-916-678-85-17",
         "email": "ossminoga88@gmail.com",
     }
-    context = {
-        'author': author,
-    }
+    context = {'author': author}
     return render(request, "about.html", context)
 
 
 def get_item(request, item_id: int):
-    """TODO: get item by id from db."""
-    for item in items:
-        if item['id'] == item_id:
-            context = {
-                "item": item
-            }
-            return render(request, "item_page.html", context)
-    return render(request, "errors.html", {'errors': [f'Item with id={item+id} not found']})
-        
+    """Get item by id from db."""
+    try:
+        item = Item.objects.get(id=item_id)
+    except ObjectDoesNotExist:
+        return render(request, "errors.html", {'errors': [f'Item with id={item+id} not found']})
+    else:
+        context = {"item": item}
+        return render(request, "item_page.html", context)
+    
 
 def get_items(request):
-    """TODO: get all items from db."""
-    context = {"items": items }
+    """Get all items from db."""
+    context = {"items": Item.objects.all()}
     return render(request, "items_list.html", context)
 
 
